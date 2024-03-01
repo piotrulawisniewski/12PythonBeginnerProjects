79d7a36ed1d9266f3c47b2d98763f0a9d6b2bfa6
R32NOR <piotrulawisniewski@gmail.com>
2024-03-01 13:20:48 +0100

    updated game
    added few small featrues

--- 05_hangman/05_Hangman.py
+++ 05_hangman/05_Hangman.py
@@ -5,7 +5,7 @@ print('Welcome to hangman game!')
 
 # random choose of noun from list:
 wordToGuess= random.choice(words)
-
+wordToGuess=wordToGuess.lower()
 
 # dividing word for letters and assemble it to to the
 lettersToGuess = [letter for letter in wordToGuess]
@@ -15,28 +15,55 @@ noOfLives = int(wordLength)
 
 print(wordToGuess)
 print(lettersToGuess)
+print('Number of lives: ' , noOfLives)
 
+# hiding the letters (user knows only the number of letters):
 shownWord='_'*wordLength
 print(shownWord)
 
-
+# list for current situation- to shown letter if that has been guessed and underscore if not:
 currentLetters=[lett for lett in shownWord]
-# container for letters that have been used by user:
+
+# list for letters that have been used by user:
 usedLetters = []
 
-n=0
-i=0
-
-while True:
+i = 0
+while noOfLives > 0:
     guessedLetter=input('\nGuess the letter: ')
+    guessedLetter=guessedLetter.lower().strip()
+    if guessedLetter in usedLetters:
+        while guessedLetter in usedLetters:
+            guessedLetter=input('\nYou have tried this letter. Try other one:  ')
+            guessedLetter = guessedLetter.lower().strip()
+
     usedLetters.append(guessedLetter)
-    for i in range(0,len(lettersToGuess)):
-        if lettersToGuess[i] in usedLetters:
-            currentLetters[i]=lettersToGuess[i]
-        else:
-            currentLetters[i] = '_'
-        i+=1
-    print(currentLetters)
+    usedLetters.sort()
+
+    # checking if chosen letter in seeking word:
+    if guessedLetter in lettersToGuess:
+        for i in range(0,len(lettersToGuess)):
+            if lettersToGuess[i] in usedLetters:
+                currentLetters[i]=lettersToGuess[i]
+            i+=1
+    else:
+        noOfLives = noOfLives - 1
+
+    # checking if word is guessed:
+    word=''
+    for singleLetter in currentLetters:
+        word+=singleLetter
+
+    if word == wordToGuess:
+        print('Letters that have been used:', usedLetters)
+        print('You have won! The word was:\n',word.upper())
+        break
+    else:
+        print('Letters that have been used:', usedLetters,'\n')
+        print('Word to guess:', currentLetters)
+        print(noOfLives, ' lives remains')
+else:
+    print('\nGame Over!\nPlay again.')
+
 
 
 
